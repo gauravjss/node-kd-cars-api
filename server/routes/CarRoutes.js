9853a4a49a1267330b17e55e24b91ea70cc9ca17
Gaurav Srivastava <gauravjss@gmail.com>
2018-07-04 23:47:38 -0500

    Changing Mock Data

--- server/routes/CarRoutes.js
+++ server/routes/CarRoutes.js
@@ -12,10 +12,7 @@ exports.getCarRoute = (req,res) => {
 }
 
 exports.getCarDriverRoute = (req,res) => {
-    res.type('json');
-    res.append('Set-Cookie', 'foo=bar; Path=/; HttpOnly');
-    res.append('Warning', '199 Miscellaneous warning');
-    res.json(carDriverJsonFile);
+    res.type('json').append('Set-Cookie', 'foo=bar; Path=/; HttpOnly'.append('Warning', '199 Miscellaneous warning').status(5500).url('www.kd.com').headers({'make':'mercedes'}).json(carDriverJsonFile);
 }
 
 exports.getAirlinesRoute = (req,res) => {
