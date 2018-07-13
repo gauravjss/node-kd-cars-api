f7b56620449da6d2d9cd85ece3bf0e6f87c63b8a
Gaurav Srivastava <gauravjss@gmail.com>
2018-07-13 14:15:07 -0500

    Adding template view

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const hbs = require('hbs');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -9,6 +10,20 @@ var {mongoose} = require('./db/mongoose');
 var app = express();
 const port = process.env.PORT || 3000;
 
+// Setting up the Partial Directory to Handlebar
+hbs.registerPartials(__dirname + '../../views/partials');
+
+
+// Helper without Args.
+hbs.registerHelper('getCurrentYear',()=> new Date().getFullYear());
+
+// Helper with Parameters
+hbs.registerHelper('screamIt' ,(text)=> text.toUpperCase());
+
+
+// Setting Up View Engine as Handlebar
+app.set('view engine or anything else','hbs');
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -16,6 +31,14 @@ app.use(function(req, res, next) {
 });
 
 app.use(bodyParser.json());
+
+app.get('/',(req,res) =>{
+    res.render('home.hbs',{
+        pageTitle: 'Welcome Page',
+        welcomeMessage: `This is the ReadMe page for the API's `
+    })
+});
+
 
 app.post('/todos', TodoRoutes.postRoute);
 app.get('/todos', TodoRoutes.getRoute);
